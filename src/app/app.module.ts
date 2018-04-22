486541ab35e672b3d8eb191406b37ab72a06672d
Alexander S. Makarevich <alexander.makarevich@eltex.loc>
2018-04-22 10:42:10 +0700

    Доступна страница "projects" с помощью RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,10 @@ import { ProjectsComponent } from './components/projects/projects.component';
     ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'projects', component: ProjectsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
