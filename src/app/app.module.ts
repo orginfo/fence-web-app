710ce1204c926a0e4861f99d87903a9f270d9e2a
Alexander S. Makarevich <alexander.makarevich@eltex.loc>
2018-05-10 08:48:27 +0700

    Добавлен маршрут по-умолчанию

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { RegionOfType2Component } from './components/region-of-type-2/region-of-
       { path: 'projects/:projectId/new-region-of-type-1', component: RegionOfType1Component },
       { path: 'projects/:projectId/regions-of-type-2/:regionId', component: RegionOfType2Component },
       { path: 'projects/:projectId/new-region-of-type-2', component: RegionOfType2Component },
+      { path: '', redirectTo: '/projects', pathMatch: 'full' },
     ])
   ],
   providers: [ProjectService, RegionTypeService],
