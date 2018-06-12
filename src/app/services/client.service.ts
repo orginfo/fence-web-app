a89505828f3e335e50f8080b5e8422609592888c
Alexander S. Makarevich <alexander.makarevich@eltex.loc>
2018-06-12 17:44:21 +0700

    refs #40: ClientService.searchClients возвращает данные от сервера

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { ApiClient, CLIENTS } from '../models/apiclient';
+import { ApiClient } from '../models/apiclient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+  private BASE_URL: string = 'http://api.localhost:8080/v0';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   searchClients(term: string): Observable<ApiClient[]> {
     if (!term.trim()) {
       return of([]);
     }
 
-    let clients = CLIENTS.filter(client => client.name.search(term) != -1);
-    return of(clients);
+    let url: string = `${this.BASE_URL}/clients`;
+    return this.httpClient.get<any>(url).pipe(
+      map(response => response.Result.filter(client => client.name.search(term) != -1)),
+    );
   }
 }
