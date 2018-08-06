3439e3fbcab5fe5f5cd4f1762ffb0754f0257a19
Alexander S. Makarevich <alexander.s.makarevich@gmail.com>
2018-08-06 20:38:58 +0700

    refs #43: Начало экспериментов

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, throwError } from 'rxjs';
 import {
-   debounceTime, distinctUntilChanged, switchMap
+   debounceTime, distinctUntilChanged, switchMap, catchError
  } from 'rxjs/operators';
 
 import { ProjectService } from '../../services/project.service';
 import { ClientService } from '../../services/client.service';
 import { ApiClient } from '../../models/apiclient';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-project',
@@ -56,8 +57,28 @@ export class ProjectComponent implements OnInit {
     });
   }
 
+  private handleError(error: HttpErrorResponse) {
+    debugger;
+  if (error.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    console.error('An error occurred:', error.error.message);
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    console.error(
+      `Backend returned code ${error.status}, ` +
+      `body was: ${error.error}`);
+  }
+  // return an observable with a user-facing error message
+  return throwError(
+    'Something bad happened; please try again later.');
+};
+
   onSubmit() {
       this.projectService.addProject(this.projectForm.value).
+        pipe(
+          catchError(this.handleError)
+        ).
         subscribe(response => {console.log(response)});
   }
 
